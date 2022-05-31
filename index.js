7e84f57ad1d7c643218efb42425da706a15142ab
Naomi Cole <naomisarahcole@gmail.com>
2022-05-31 17:49:10 +1000

    Completed assignments

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solutions in this file
+const names = ["Rhys", "Dad", "Mum", "Sean", "Brendan"];
+
+function writeCards(names) {
+  for (let i = 0; i < names.length; i++) {
+    console.log(`Thank you,  ${names[i]} for the wonderful birthday gift!`);
+    debugger;
+  }
+
+  return names;
+}
+
+writeCards(names); 
+
+
+function countDown () {
+    let count = 10;
+    while (count >= 0) {
+        console.log(count--);
+        debugger;
+    }
+}
+
+countDown();
